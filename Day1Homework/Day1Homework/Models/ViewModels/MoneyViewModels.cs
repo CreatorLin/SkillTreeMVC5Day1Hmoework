dcf299140f923fa3f1200a227ea178fe3cd958cc
Creator <Creator@HOME-BANQIAO>
2016-08-02 15:45:58 +0800

    New MoneyViewModels

---
+++ Day1Homework/Day1Homework/Models/ViewModels/MoneyViewModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Day1Homework.Models.ViewModels
+{
+    public enum MoneyCategory
+    {
+        收入 = 1,
+        支出
+    }
+
+    public class MoneyViewModel
+    {
+        public MoneyCategory Category { get; set; }
+        public decimal Money { get; set; }
+        public string Date { get; set; }
+        public string Description { get; set; }
+    }
+}
