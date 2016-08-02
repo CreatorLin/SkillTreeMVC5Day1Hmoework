fc7188af2b41ced62c841f6cfdbc2e13412934d6
Creator <Creator@HOME-BANQIAO>
2016-08-02 15:36:59 +0800

    New MoneyController

---
+++ Day1Homework/Day1Homework/Controllers/MoneyController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day1Homework.Controllers
+{
+    public class MoneyController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
