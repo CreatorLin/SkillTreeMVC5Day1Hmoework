ee81f5f01a667d45afb2882f811222a44be8775e
Creator Lin <Creator Lin>
2016-08-07 01:32:01 +0800

    Add AccountBookManager (= Service)

--- Homework1/Homework1/Controllers/MoneyController.cs
+++ Homework1/Homework1/Controllers/MoneyController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Homework1.Managers;
+using Homework1.Models;
+using Homework1.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +11,15 @@ namespace Homework1.Controllers
 {
     public class MoneyController : Controller
     {
+        private AccountBookManager manager;
+
+        public MoneyController()
+        {
+            var dbContext = new MyEntity();
+
+            manager = new AccountBookManager(dbContext);
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -22,7 +34,14 @@ namespace Homework1.Controllers
         [ChildActionOnly]
         public ActionResult List()
         {
-            return PartialView(SampleData.MoneySampleData.Get());
+            var viewData = manager.Lookup().Select(p => new MoneyViewModel()
+            {
+                Category = (MoneyCategory)p.Categoryyy,
+                Money = p.Amounttt,
+                Date = p.Dateee
+            }).ToList();
+            
+            return PartialView(viewData);
         }
     }
 }
---
+++ Homework1/Homework1/Managers/AccountBookManager.cs
@@ -0,0 +1,28 @@
+﻿using Homework1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Homework1.Managers
+{
+    public class AccountBookManager
+    {
+        private MyEntity db;
+
+        public AccountBookManager(MyEntity dbContext)
+        {
+            db = dbContext;
+        }
+
+        public IQueryable<AccountBook> Lookup()
+        {
+            return db.AccountBooks;
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+    }
+}
