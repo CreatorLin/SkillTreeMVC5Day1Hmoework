d52631d03f9bd17adae37b0bc98ff16b1e9ba2d7
Creator <Creator@home>
2016-09-02 16:43:52 +0800

    金額必須用選的。

--- Homework1/Homework1/Scripts/App.js
+++ Homework1/Homework1/Scripts/App.js
@@ -3760,3 +3760,15 @@ $(document).ready(function () {
 function dateTimePicker(e) {
     e.datetimepicker();
 }
+$(document).ready(function () {
+    $('[data-increment]').each(function () {
+        increment($(this), $(this).data("increment"), $(this).data("increment-num"));
+    });
+});
+function increment(e, targetId, number) {
+    e.click(function () {
+        var value = parseInt($("#" + targetId).val()) || 0;
+        $("#" + targetId).val(value + parseInt(number));
+    });
+}
+//# sourceMappingURL=Increment.js.map
