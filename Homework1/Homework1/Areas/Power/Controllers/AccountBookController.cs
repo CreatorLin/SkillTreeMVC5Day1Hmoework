c6eedc09c8bbd978b48b1762f9c2b8bfb1272e66
Creator <Creator@home>
2016-09-05 20:57:16 +0800

    增加管理專用的 Area

---
+++ Homework1/Homework1/Areas/Power/Controllers/AccountBookController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework1.Areas.Power.Controllers
+{
+    public class AccountBookController : Controller
+    {
+        // GET: Power/AccountBook
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
