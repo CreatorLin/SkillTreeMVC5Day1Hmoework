0c0e422a833d65f421fbd201bd8db5b90668b178
Creator <Creator@home>
2016-09-05 23:15:00 +0800

    將記帳簿路由改為改為 SkillTree

--- Homework1/Homework1/App_Start/RouteConfig.cs
+++ Homework1/Homework1/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace Homework1
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "SkillTree",
+                url: "skillTree",
+                defaults: new { controller = "Money", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Money", action = "Index", id = UrlParameter.Optional }
